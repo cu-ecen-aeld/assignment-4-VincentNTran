4fd28b05bf0ff582614e1a83d4d0695170f0f70c
vinnee <vinnee@ubuntu.myguest.virtualbox.org>
2025-04-06 23:01:58 -0600

    fixed typo

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 9cd1799e39a52f5343f8f1effe6bf013882ae4b
+AESD_ASSIGNMENTS_VERSION = 9cd1799e39a52f5343f8f1effe6bf01b3882ae4b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
